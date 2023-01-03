9f861ac0faddd993ce3b78b818aa1ba41c25bd88
OsmarG <osmar_gimenez@outlook.com>
2023-01-02 23:01:02 -0300

    agregue algunas lineas de como cambiar el estilo de un elemento con js

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ var animalImg = document.querySelector("#fav-animal");
 
 console.log(animalImg);
 
-function pickCat() {
+function pickCat(element) {//al asignarle esta propiedad y dandole this a nuestros eleementos en html nos ahorramos 
+    //tener que escribir variables como var animalImg etc
+    element.style.backgroundColor = "red"// con esto cambiamos una propiedad css con js
+    //element.remove();
     animalImg.src = "cat.jpg";
     console.log(animalImg.src);
+    console.log(element);
 }
 
-function pickDog() {
+function pickDog(element) {
+    element.classList.add("btn")
     animalImg.src = "dog.jpg";
     console.log(animalImg.src);
+    console.log(element);
 }
